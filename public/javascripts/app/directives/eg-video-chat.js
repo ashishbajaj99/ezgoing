46d1b37074040329c5e17a7ef340c63d3f36b220
ashishbajaj99 <bajaj.ashish@gmail.com>
2014-12-20 13:19:49 +0530

    test webrtc

---
+++ public/javascripts/app/directives/eg-video-chat.js
@@ -0,0 +1,27 @@
+(function() {
+    var egVideoChat = angular.module('EgVideoChat');
+
+    egVideoChat.directive('egVideoChat', function() {
+        return {
+            restrict: 'E',
+            scope: true,
+            templateUrl: '../../../views/videochat.html',      
+            controller: [ '$scope', '$attrs', function($scope, $attrs) {
+                var webrtc = new SimpleWebRTC({
+                                                // the id/element dom element that will hold "our" video
+                                                localVideoEl: 'localVideo',
+                                                // the id/element dom element that will hold remote videos
+                                                remoteVideosEl: 'remotesVideos',
+                                                // immediately ask for camera access
+                                                autoRequestMedia: true
+                                            });
+                // we have to wait until it's ready
+                webrtc.on('readyToCall', function () {
+                    // you can name it anything
+                    webrtc.joinRoom('ashish');
+                });
+            }]
+        };
+    });
+
+})();    
